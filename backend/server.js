ebb9297312e18c54e03cff7b2288b7781a727e72
tashwini-p <tashwiniprakasha@gmail.com>
2024-02-21 18:59:54 +0530

    server.js changed

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
-const jsonServer = require("json-server"); // importing json-server library
+const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
 
+server.db = router.db;
+
+// Set up the authentication middleware
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+const port = process.env.PORT || 4000;
+
+server.listen(port, () => {
+  console.log(`JSON Server with authentication is running on port ${port}`);
+});
